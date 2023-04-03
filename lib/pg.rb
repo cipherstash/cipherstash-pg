30325a56c1fa3a8f3d8eb4ff5b72e84c1c19e0c8
James Sadler <james@cipherstash.com>
2023-04-03 10:26:16 +1000

    The pg_ext extension is now versioned in the filename instead of a subdir

--- lib/pg.rb
+++ lib/pg.rb
@@ -10,7 +10,7 @@ module PG
 	major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
 		raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
 	begin
-		require "#{major_minor}/pg_ext"
+		require "pg_ext-#{major_minor}"
 	rescue => e
 	        STDERR.puts "Failed to load pg_ext for #{RUBY_VERSION.dump}"
 	        exit 1
